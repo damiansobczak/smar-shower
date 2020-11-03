6d212d14e515a5f808669ba184d71570c3c35977
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2020-11-03 18:54:21 +0100

    Removed unneccessary keyboard events

--- src/components/Mode/Mode.tsx
+++ src/components/Mode/Mode.tsx
@@ -7,45 +7,27 @@ export default function Mode() {
   const state = useSelector((state: IState) => state.isDark);
   const dispatch = useDispatch();
 
-  const setToLight = (e: React.SyntheticEvent | React.KeyboardEvent) => {
-    if (("key" in e && e.key === "Enter") || e.type === "click") {
-      dispatch({ type: "LIGHT" });
-    }
-    e.stopPropagation();
-  };
-
-  const setToDark = (e: React.SyntheticEvent | React.KeyboardEvent) => {
-    if (("key" in e && e.key === "Enter") || e.type === "click") {
-      dispatch({ type: "DARK" });
-    }
-    e.stopPropagation();
-  };
-
   return (
-    <ul id="mode" className="mode" role="radiogroup" aria-label="Theme mode">
-      <li
-        id="mode__item"
-        className="mode__item"
-        aria-label="Light"
-        role="radio"
-        aria-checked={state ? false : true}
-        onClick={setToLight}
-        onKeyPress={setToLight}
-        tabIndex={0}
-      >
-        <i className={`mode__icon icon-sun ${!state ? "mode__icon--active" : ""}`}></i>
+    <ul id="mode" className="mode" aria-label="Theme mode">
+      <li id="mode__item" className="mode__item">
+        <button
+          className={`mode__btn ${!state ? "mode__btn--active" : ""}`}
+          onClick={() => dispatch({ type: "LIGHT" })}
+          aria-label="Light"
+          aria-pressed={state ? false : true}
+        >
+          <i className="mode__icon icon-sun"></i>
+        </button>
       </li>
-      <li
-        id="mode__item"
-        className="mode__item"
-        aria-label="Dark"
-        role="radio"
-        aria-checked={state ? true : false}
-        onClick={setToDark}
-        onKeyPress={setToDark}
-        tabIndex={0}
-      >
-        <i className={`mode__icon icon-moon ${state ? "mode__icon--active" : ""}`}></i>
+      <li id="mode__item" className="mode__item">
+        <button
+          className={`mode__btn ${state ? "mode__btn--active" : ""}`}
+          onClick={() => dispatch({ type: "DARK" })}
+          aria-label="Dark"
+          aria-pressed={state ? true : false}
+        >
+          <i className="mode__icon icon-moon"></i>
+        </button>
       </li>
     </ul>
   );
--- src/components/NavItem/NavItem.tsx
+++ src/components/NavItem/NavItem.tsx
@@ -3,16 +3,9 @@ import { INavItem } from "./INavItem";
 import "./NavItem.scss";
 
 export default function NavItem({ id, active, name, icon, setActive }: INavItem) {
-  const hangleSetActive = (e: React.SyntheticEvent | React.KeyboardEvent, id: number) => {
-    if (("key" in e && e.key === "Enter") || e.type === "click") {
-      setActive(id);
-    }
-    e.stopPropagation();
-  };
-
   return (
     <li className={`nav__item ${active ? "nav__item--active" : ""}`}>
-      <button className="nav__link" onClick={(e) => hangleSetActive(e, id)} onKeyDown={(e) => hangleSetActive(e, id)}>
+      <button className="nav__link" onClick={() => setActive(id)}>
         <i className={`nav__icon icon-${icon}`}></i>
         <span>{name}</span>
       </button>
