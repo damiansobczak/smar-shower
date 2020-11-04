0f7166ff0b882c4578c1493826008bc9e0c7b384
Damian Sobczak <damiansobczak@MacBook-Pro-Damian.local>
2020-11-04 17:16:17 +0100

    Added logout component

--- src/components/Logout/Logout.tsx
+++ src/components/Logout/Logout.tsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Logout.scss";
 
 export default function Logout() {
-  return <button className="logout">logout</button>;
+  return (
+    <button className="logout">
+      <i className="icon-power-off"></i>
+    </button>
+  );
 }
